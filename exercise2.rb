442c834d4363f5ba530c05de930d2836dac542a3
mitchell demers <mitchelldemers66@gmail.com>
2016-08-09 10:51:45 -0400

    working exercise2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+def calc_tip(meal_total)
+	tip_amount = meal_total * 0.15
+	puts "on a bill of #{meal_total}, you should tip #{tip_amount}"
+end	
+
+calc_tip(55)
+
+my_age = 23
+
+puts "my age is #{my_age}"  
+
+puts "the result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11) # this equals true bec one of the two statements are correct
